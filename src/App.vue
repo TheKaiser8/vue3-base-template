7fc57c320f625d02272b8f68c65d656d41cdf3bf
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-24 16:36:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <AppTitle/>
+  </div>
+  <div class="container-small">
+    <AppTitle/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style/global.scss';
+</style>
